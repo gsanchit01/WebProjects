391a02f691ee1388322ee18ea0a5570df992f7be
gsanchit01 <70408013+gsanchit01@users.noreply.github.com>
2020-10-01 01:02:17 +0530

    Contact us file
    The contact form will work with the help of contact.php file

---
+++ contact.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+<?php 
+
+       $name = $_POST['name'];
+       $email = $_POST['email'];
+       $subject = 'New Form Submission';
+       $message = $_POST['message'];
+
+            $to = "jathinp74@gmail.com";
+               
+           if(mail($to,$subject,"Name: ".$name."\nMessage: ".$message,$email))
+           {
+                echo '<script>alert("Your Messege has been sent")</script>'; 
+           }
+           else
+           {
+              echo '<script>alert("Your Messege has some Error\n *Please Try Again*")</script>'; 
+           }
+
+?>
+</body>
+<head><meta http-equiv = "refresh" content = "0; url = contact.html" /></head>
+</html>
